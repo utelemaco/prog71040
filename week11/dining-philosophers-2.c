f5cea861a3f8b0535469c1f30024bac846c81118
Ulisses Telemaco <ulisses.telemaco@gmail.com>
2025-07-16 17:27:37 -0400

    Fixing deadlock situation

--- week11/dining-philosophers-2.c
+++ week11/dining-philosophers-2.c
@@ -3,100 +3,75 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <time.h>
-#include <errno.h>
 
 #define NUM_PHILOSOPHERS 5
-#define THINKING_TIME 2
-#define EATING_TIME 1
+#define EATING_TIME 2
+#define THINKING_TIME 3
 
 // Mutex for each fork
 pthread_mutex_t forks[NUM_PHILOSOPHERS];
 
-// Mutex for printing to avoid garbled output
-pthread_mutex_t print_mutex;
+// Mutex for printing (to avoid mixed output)
+pthread_mutex_t print_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-// Philosopher states for visualization
-enum State { THINKING, HUNGRY, EATING };
-enum State philosopher_states[NUM_PHILOSOPHERS];
+// Philosopher states
+enum State {
+    THINKING,
+    HUNGRY,
+    EATING
+};
 
-void print_state(int id, const char* action) {
+// Function to print philosopher state
+void print_state(int id, enum State state) {
     pthread_mutex_lock(&print_mutex);
-    printf("Philosopher %d is %s\n", id, action);
-    pthread_mutex_unlock(&print_mutex);
-}
-
-void print_table_state() {
-    pthread_mutex_lock(&print_mutex);
-    printf("\nTable State: ");
-    for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-        switch (philosopher_states[i]) {
-            case THINKING: printf("T "); break;
-            case HUNGRY: printf("H "); break;
-            case EATING: printf("E "); break;
-        }
+    
+    switch(state) {
+        case THINKING:
+            printf("Philosopher %d is thinking...\n", id);
+            break;
+        case HUNGRY:
+            printf("Philosopher %d is hungry and trying to pick up forks\n", id);
+            break;
+        case EATING:
+            printf("Philosopher %d is eating\n", id);
+            break;
     }
-    printf("\n");
+    
     pthread_mutex_unlock(&print_mutex);
 }
 
-void think(int id) {
-    philosopher_states[id] = THINKING;
-    print_state(id, "thinking");
-    sleep(rand() % THINKING_TIME + 1);
-}
-
-void eat(int id) {
-    philosopher_states[id] = EATING;
-    print_state(id, "eating");
-    print_table_state();
-    sleep(rand() % EATING_TIME + 1);
-}
-
+// Philosopher function
 void* philosopher(void* arg) {
     int id = *(int*)arg;
     int left_fork = id;
     int right_fork = (id + 1) % NUM_PHILOSOPHERS;
     
-    // To prevent deadlock, we use a simple ordering strategy:
-    // Always pick up the lower-numbered fork first
-    int first_fork = right_fork;//(left_fork < right_fork) ? left_fork : right_fork;
-    int second_fork = left_fork;//(left_fork < right_fork) ? right_fork : left_fork;
+    // To prevent deadlock, philosopher with higher ID picks up right fork first
+    int first_fork = (id < right_fork) ? left_fork : right_fork;
+    int second_fork = (id < right_fork) ? right_fork : left_fork;
     
-    while (1) {
+    while(1) {
         // Think
-        think(id);
+        print_state(id, THINKING);
+        sleep(rand() % THINKING_TIME + 1);
         
         // Get hungry
-        philosopher_states[id] = HUNGRY;
-        print_state(id, "hungry - trying to pick up forks");
+        print_state(id, HUNGRY);
         
         // Pick up forks (in order to prevent deadlock)
-        printf("Philosopher %d is trying to pick up fork %d\n", id, first_fork);
         pthread_mutex_lock(&forks[first_fork]);
-        printf("Philosopher %d picked up fork %d\n", id, first_fork);
-        
-        sleep(rand() % EATING_TIME + 1);
-        
-        printf("Philosopher %d is trying to pick up fork %d\n", id, second_fork);
-        while (pthread_mutex_trylock(&forks[second_fork]) == EBUSY) {
-            printf("Fork %d is in use. Philosopher %d is waiting to pick up it\n", second_fork, id);
-            sleep(rand() % EATING_TIME + 1);
-        }
-
         pthread_mutex_lock(&forks[second_fork]);
-        printf("Philosopher %d picked up fork %d\n", id, second_fork);
         
         // Eat
-        eat(id);
+        print_state(id, EATING);
+        sleep(rand() % EATING_TIME + 1);
         
         // Put down forks
         pthread_mutex_unlock(&forks[second_fork]);
-        printf("Philosopher %d put down fork %d\n", id, second_fork);
-        
         pthread_mutex_unlock(&forks[first_fork]);
-        printf("Philosopher %d put down fork %d\n", id, first_fork);
         
-        print_state(id, "finished eating");
+        // Brief pause before next cycle
+        usleep(100000); // 0.1 second
     }
     
     return NULL;
@@ -109,34 +84,34 @@ int main() {
     // Initialize random seed
     srand(time(NULL));
     
-    // Initialize mutexes
-    for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-        pthread_mutex_init(&forks[i], NULL);
-        philosopher_states[i] = THINKING;
-    }
-    pthread_mutex_init(&print_mutex, NULL);
-    
-    printf("Dining Philosophers Problem\n");
-    printf("==========================\n");
-    printf("5 philosophers sitting around a table\n");
-    printf("States: T=Thinking, H=Hungry, E=Eating\n\n");
-    
-    // Create philosopher threads
-    for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-        philosopher_ids[i] = i;
-        if (pthread_create(&philosophers[i], NULL, philosopher, &philosopher_ids[i]) != 0) {
-            perror("Failed to create philosopher thread");
-            exit(1);
+    // Initialize fork mutexes
+    for(int i = 0; i < NUM_PHILOSOPHERS; i++) {
+        if(pthread_mutex_init(&forks[i], NULL) != 0) {
+            fprintf(stderr, "Failed to initialize mutex for fork %d\n", i);
+            return 1;
         }
     }
     
-    // Wait for all philosophers (this will run indefinitely)
-    for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+    printf("=== Dining Philosophers Problem ===\n");
+    printf("Number of philosophers: %d\n", NUM_PHILOSOPHERS);
+    printf("Starting simulation...\n\n");
+    
+    // Create philosopher threads
+    for(int i = 0; i < NUM_PHILOSOPHERS; i++) {
+        philosopher_ids[i] = i;
+        if(pthread_create(&philosophers[i], NULL, philosopher, &philosopher_ids[i]) != 0) {
+            fprintf(stderr, "Failed to create thread for philosopher %d\n", i);
+            return 1;
+        }
+    }
+    
+    // Wait for all threads to complete (they won't in this infinite loop)
+    for(int i = 0; i < NUM_PHILOSOPHERS; i++) {
         pthread_join(philosophers[i], NULL);
     }
     
-    // Cleanup mutexes (this code won't be reached in this infinite loop version)
-    for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+    // Clean up mutexes
+    for(int i = 0; i < NUM_PHILOSOPHERS; i++) {
         pthread_mutex_destroy(&forks[i]);
     }
     pthread_mutex_destroy(&print_mutex);
